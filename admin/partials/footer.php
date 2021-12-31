b51129012aa4f05fdb4c17674a434c4917d62c17
Santiljano Malaj <67653807+Santiljano@users.noreply.github.com>
2021-12-31 22:03:08 +0100

    Add files via upload

---
+++ admin/partials/footer.php
@@ -0,0 +1,12 @@
+ <!---Footer Section Starts-->
+ <div class="footer">
+        <div class="wrapper">
+                <p class="text-center">2021 All rights reserved, Food House . Developed by <a href="#">Santiljano Malaj</a> </p>
+
+            </div>
+        </div>
+        <!---Footer Section Ends--->
+
+
+    </body>
+</html>
